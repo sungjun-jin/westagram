8327267cbb8631b576764b2520605c52ab85bb6e
Sung Jun Jin <Sung Jun Jin>
2020-04-07 13:25:47 +0900

    user app 1차 수정

--- user/models.py
+++ user/models.py
@@ -3,26 +3,14 @@ from django.db import models
 # 사용자
 class User(models.Model) :
 
-    email      = models.EmailField(max_length=30)
-    password   = models.CharField(max_length=30)
+    email      = models.EmailField(max_length=200)
+    password   = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta :
 
         db_table = 'users'
-
-# 댓글
-class Comment(models.Model) :
-
-    user_email = models.ForeignKey(User,on_delete=models.CASCADE)
-    comment    = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta :
-        db_table = 'comments'
-
 
 
 
--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import SignUpView,SignInView,CommentView
+from .views import SignUpView,SignInView
 
 urlpatterns = [
 
    path('/sign-up', SignUpView.as_view()),
    path('/sign-in', SignInView.as_view()),
-   path('/comment',CommentView.as_view()),
-   # 댓글 GET Method
-   path('/comment/user_id=<id>',CommentView.as_view()),    
 ]
